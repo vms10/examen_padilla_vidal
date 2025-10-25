1d0694e8c34687906f3a08035c01855534aeee3b
marcopadilla <sebastian.marco@gmail.com>
2025-10-25 11:45:50 -0300

    subiendo creacion de endpoints

--- main.py
+++ main.py
@@ -1,62 +1,119 @@
-# archivo principal
-import os
-from fastapi import FastAPI, File, UploadFile,HTTPException
-from fastapi.responses import FileResponse
-from typing import Annotated
+import json
+from fastapi import FastAPI
+
+STATUS = "status"
+AMOUNT = "amount"
+PAYMENT_METHOD = "payment_method"
+
+STATUS_REGISTRADO = "REGISTRADO"
+STATUS_PAGADO = "PAGADO"
+STATUS_FALLIDO = "FALLIDO"
+
+DATA_PATH = "data.json"
+
 app = FastAPI()
 
-@app.get("/hola")
+# @app.get("/")
+# @app.post("/CrearArchivo/")
+# @app.get("/LeerArchivo/{file_name}")
+
+
+def load_all_payments():
+    with open(DATA_PATH, "r") as f:
+        data = json.load(f)
+    return data
+
+
+def save_all_payments(data):
+    with open(DATA_PATH, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def load_payment(payment_id):
+    data = load_all_payments()[payment_id]
+    return data
+
+
+def save_payment_data(payment_id, data):
+    all_data = load_all_payments()
+    all_data[str(payment_id)] = data
+    save_all_payments(all_data)
+
+
+def save_payment(payment_id, amount, payment_method, status):
+    data = {
+        AMOUNT: amount,
+        PAYMENT_METHOD: payment_method,
+        STATUS: status,
+    }
+    save_payment_data(payment_id, data)
+
+
+# Probando la api
+@app.get("/")
 async def prueba_root():
     return {"message": "Accediste al endpoint de prueba"}
 
-@app.get("/files")
-async def buscar_archivos():
-    carpeta = "files"
+# GET en el path /payments que retorne todos los pagos.
+@app.get("/payments")
+async def get_all_payments():
+    data = {"data": load_all_payments()}
+    return data
 
-    # Verifica que la carpeta exista
-    if not os.path.exists(carpeta):
-        return {"error": f"La carpeta '{carpeta}' no existe."}
+# POST en el path /payments/{payment_id} que registre un nuevo pago.
+@app.post("/payments/{payment_id}")
+async def create_payment(payment_id: str, amount: float, payment_method: str):
+    save_payment(payment_id, amount, payment_method, STATUS_REGISTRADO)
+    return {"message": f"Pago {payment_id} registrado con estado {STATUS_REGISTRADO}"}
 
-    # Lista solo los archivos dentro de la carpeta
-    archivos = [
-        f for f in os.listdir(carpeta)
-        if os.path.isfile(os.path.join(carpeta, f))
-    ]
-    return {"archivos": archivos}
+# POST en el path /payments/{payment_id}/update que cambie los parametros de una pago (amount, payment_method)
+@app.post("/payments/{payment_id}/update")
+async def update_payment(payment_id: str, amount: float = None, payment_method: str = None, status: str = None):
+    data = load_payment(payment_id)
 
+    # if data is None:
+    #     return {"error": f"Pago {payment_id} no encontrado"}
 
+    if data[STATUS] == STATUS_REGISTRADO:
+        if amount is not None:
+            data[AMOUNT] = amount
+        if payment_method is not None:
+            data[PAYMENT_METHOD] = payment_method
+        if status is not None:
+            data[STATUS] = status
 
+        save_payment_data(payment_id, data)
+        return {"message": f"Pago {payment_id} actualizado", "data": data}
+    else:
+        return {"error": f"Pago {payment_id} no puede ser actualizado"}
 
+# POST en el path /payments/{payment_id}/pay que intente.
+@app.post("/payments/{payment_id}/pay")
+async def pay_payment(payment_id: str):
+    data = load_payment(payment_id)
 
+    # if data is None:
+    #     return {"error": f"Pago {payment_id} no encontrado"}
 
-# Endpoint que guarda el archivo en la carpeta "files"
-@app.post("/files")
-async def upload_file(
-    file: Annotated[UploadFile, File(...)]
-):
-    # Asegurarte de que exista la carpeta "files"
-    os.makedirs("files", exist_ok=True)
+    if data[STATUS] == STATUS_REGISTRADO:
+        data[STATUS] = STATUS_PAGADO
+        save_payment_data(payment_id, data)
+        return {"message": f"Pago {payment_id} procesado", "data": data}
+    else:
+        return {"error": f"Pago {payment_id} no puede ser procesado"}
+    
 
-    # Ruta completa donde se va a guardar
-    save_path = os.path.join("files", os.path.basename(file.filename))
+# POST en el path /payments/{payment_id}/revert que revertir el pago.
+@app.post("/payments/{payment_id}/revert")
+async def revert_payment(payment_id: str):
+    data = load_payment(payment_id)
 
-    # Leer el contenido del archivo y escribirlo
-    with open(save_path, "wb") as f:
-        content = await file.read()
-        f.write(content)
+    # if data is None:
+    #     return {"error": f"Pago {payment_id} no encontrado"}
 
-    return {"message": f"Archivo guardado como {save_path}", "size": len(content)}
-
-# te descarga el archivo
-@app.get("/files/{file_name}")
-async def contenido_archivo(file_name:str):
-    path = os.path.join("files", file_name)  # carpeta hardcodeada
-    if not os.path.exists(path) or not os.path.isfile(path):
-        raise HTTPException(status_code=404, detail="Archivo no encontrado")
-
-    # Fuerza descarga con nombre del archivo
-    return FileResponse(path, media_type="application/octet-stream", filename=file_name)
-
-
-
-
+    if data[STATUS] == STATUS_FALLIDO:
+        data[STATUS] = STATUS_REGISTRADO
+        save_payment_data(payment_id, data)
+        return {"message": f"Pago {payment_id} revertido", "data": data}
+    else:
+        return {"error": f"Pago {payment_id} no puede ser revertido"}
